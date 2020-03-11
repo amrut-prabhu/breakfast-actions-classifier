0be5f7a3f719651e157dce0f38a5b52a0c6fa167
amad-person <e0072565@u.nus.edu>
2020-03-11 17:00:57 +0800

    Fix bug in finding max segment in test files

--- preprocessing/find_max_segment_size.py
+++ preprocessing/find_max_segment_size.py
@@ -2,20 +2,22 @@ import numpy as np
 import pandas as pd
 import glob
 
-# Maximum size of segment after processing training files:  5791 File with maximum sized segment:
-# ../groundTruth/P17_webcam01_P17_scrambledegg.txt Segment split of file with maximum sized segment:  [(16, 196),
-# (197, 645), (646, 1212), (1213, 1362), (1363, 1786), (1787, 7577), (7578, 7651), (7652, 7835)] Maximum size of
-# segment after processing test files:  5791 Index of file in segment.txt:  0
+# Maximum size of segment after processing training files:  5791
+# File with maximum sized segment:  ../groundTruth/P17_webcam01_P17_scrambledegg.txt
+# Segment split of train file with maximum sized segment:  [(16, 196), (197, 645), (646, 1212),
+# (1213, 1362), (1363, 1786), (1787, 7577), (7578, 7651), (7652, 7835)]
+# Maximum size of segment after processing test files:  3954
+# Index of file in segment.txt:  228
+# Segment split of test file with maximum sized segment:  [41, 456, 694, 1057, 2249, 2483, 2855, 6809, 6966, 7448]
 
 # get list of train .txt files
 PREFIX_PATH = "../groundTruth/"
 train_file_paths = sorted(glob.glob(PREFIX_PATH + "*.txt"))
 
 # get segment splits for train .txt files
-train_segments_file = open('train_segments.txt', 'w')
-max_segment_size = -np.inf
-max_segment_file_path = ""  # for debugging
-max_segment_file_indices = None  # for debugging
+max_segment_size_train = -np.inf
+max_segment_file_path_train = ""  # for debugging
+max_segment_file_indices_train = None  # for debugging
 for train_file_path in train_file_paths:
     print("Processing file: ", train_file_path)
     text_file = open(train_file_path, "r")
@@ -31,20 +33,21 @@ for train_file_path in train_file_paths:
     # find max segment size
     for (idx, segment_indices) in enumerate(segment_split_indices):
         curr_segment_size = segment_indices[1] - segment_indices[0] + 1
-        if curr_segment_size > max_segment_size:
-            max_segment_size = curr_segment_size
-            max_segment_file_path = train_file_path
-            max_segment_file_indices = segment_split_indices
+        if curr_segment_size > max_segment_size_train:
+            max_segment_size_train = curr_segment_size
+            max_segment_file_path_train = train_file_path
+            max_segment_file_indices_train = segment_split_indices
 
-print("Maximum size of segment after processing training files: ", max_segment_size)
-print("File with maximum sized segment: ", max_segment_file_path)
-print("Segment split of file with maximum sized segment: ", max_segment_file_indices)
-train_segments_file.close()
+print("Maximum size of segment after processing training files: ", max_segment_size_train)
+print("File with maximum sized segment: ", max_segment_file_path_train)
+print("Segment split of train file with maximum sized segment: ", max_segment_file_indices_train)
 
 # get segment splits for test files from segment.txt
+max_segment_size_test = -np.inf
 test_segments_file_path = "../segment.txt"
 test_segments_file = open(test_segments_file_path, "r")
-segment_splits = test_segments_file.read().split()
+segment_splits = test_segments_file.read().split("\n")
+max_test_segment_indices = None
 max_test_segment_file_idx = 0  # for debugging
 for (idx, segment_split) in enumerate(segment_splits):
     indices = [int(n) for n in segment_split.split()]
@@ -56,9 +59,12 @@ for (idx, segment_split) in enumerate(segment_splits):
             curr_segment_size = 1
 
         curr_segment_size += indices[i + 1] - indices[i]
-        if curr_segment_size > max_segment_size:
-            max_segment_size = curr_segment_size
+        if curr_segment_size > max_segment_size_test:
+            max_segment_size_test = curr_segment_size
+            max_test_segment_indices = indices
             max_test_segment_file_idx = idx
 
-print("Maximum size of segment after processing test files: ", max_segment_size)
-print("Index of file in segment.txt: ", max_test_segment_file_idx)  # for debugging
+print("Maximum size of segment after processing test files: ", max_segment_size_test)
+print("Index of file in segment.txt: ", max_test_segment_file_idx)
+print("Segment split of test file with maximum sized segment: ", max_test_segment_indices)
+test_segments_file.close()
